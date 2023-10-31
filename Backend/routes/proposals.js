2ea2066ca9eed33be9a9b8c7a115d5b9041e83e0
Amitabh Suyash <56742160+Asy1921@users.noreply.github.com>
2023-10-31 23:34:26 +0530

    Restructured apis and added get/getbyuname

--- Backend/routes/proposals.js
+++ Backend/routes/proposals.js
@@ -1,51 +1,20 @@
 const express = require("express");
-const mongoose = require("mongoose");
+const {
+  createProposal,
+  getProposals,
+  getProposalByUser,
+} = require("../controllers/proposalController");
 
 const router = express.Router();
-const Proposal = require("../models/proposalModel");
-const User = require("../models/userModel");
-const ObjectId = mongoose.Types.ObjectId;
 
 // get all proposals
-router.get("/", (req, res) => {
-  res.json({ mssg: "GET All Proposals" });
-});
+router.get("/", getProposals);
 
-//get single proposal
-router.get("/:id", (req, res) => {
-  res.json({ mssg: "Get single proposal" });
-});
+//get proposal by a user
+router.get("/:username", getProposalByUser);
 
 // Post a new proposal
-router.post("/", async (req, res) => {
-  let { title, description, creator, yesVotes, noVotes, executed } = req.body;
-  const user = await User.findOne({ username: creator });
-  // Access the ObjectId of the user
-  try {
-    if (user) {
-      const userId = user._id; // user._id contains the ObjectId
-      console.log(`User "${creator}" has ObjectId: ${userId}`);
-      creator = new ObjectId(user._id);
-      try {
-        const proposal = await Proposal.create({
-          title,
-          description,
-          creator,
-          yesVotes,
-          noVotes,
-          executed,
-        });
-        res.status(200).json(proposal);
-      } catch (error) {
-        res.status(400).json({ error: error.message });
-      }
-    } else {
-      console.log(`User "${creator}" not found.`);
-    }
-  } catch (err) {
-    console.error("Error finding user:", err);
-  }
-});
+router.post("/", createProposal);
 
 // Delete a new proposal
 router.delete("/:id", (req, res) => {

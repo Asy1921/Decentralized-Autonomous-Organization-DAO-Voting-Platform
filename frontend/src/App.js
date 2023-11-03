01eb9a1130ddc99f1f5e18a2bdb842134491712f
Asy1921 <amitabh.suyash@gmail.com>
2023-11-04 03:13:48 +0530

    WIP - navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,12 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./components/Homepage";
+import Navbar from "./components/navbar";
+import "bootstrap/dist/css/bootstrap.min.css";
 function App() {
   return (
     <div className="">
       <BrowserRouter>
+        <Navbar />
         <div className="pages">
           <Routes>
             <Route path="/" element={<HomePage />}></Route>
